1c73119f7a1984468ff6fa1aa5086e62bd197906
jpetzen <potocnik.1jure@gmail.com>
2023-08-19 13:36:28 +0200

    darkmode

--- app/src/main/java/si/uni_lj/fe/tnuv/oleae/Settings.kt
+++ app/src/main/java/si/uni_lj/fe/tnuv/oleae/Settings.kt
@@ -2,17 +2,22 @@ package si.uni_lj.fe.tnuv.oleae
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Switch
 import android.widget.Toast
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import com.google.firebase.auth.FirebaseAuth
-
+import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
 
 
 class Settings : PreferenceFragmentCompat () {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var sharedPrefManager: SharedPrefManager
+
+
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
@@ -36,6 +41,24 @@ class Settings : PreferenceFragmentCompat () {
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(intent)
                 Toast.makeText(requireContext(), "Logout successful", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+        // Dark mode preference
+        val darkModePreference = findPreference<SwitchPreferenceCompat>("night")
+        val sharedPreferences = requireContext().getSharedPreferences("Mode", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        darkModePreference?.setOnPreferenceChangeListener { _, newValue ->
+            val nightModeEnabled = newValue as Boolean
+            if (nightModeEnabled) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("night", true)
+                editor.apply()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("night", false)
+                editor.apply()
             }
             true
         }
